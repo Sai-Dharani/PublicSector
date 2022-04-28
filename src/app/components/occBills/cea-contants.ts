83e6885bf527621befdca450b4c465fad75c4a0b
omkargarde <omyagarde@gmail.com>
2022-04-28 11:37:49 +0530

    added bill spearatod and hr

--- src/app/components/occBills/cea-contants.ts
+++ src/app/components/occBills/cea-contants.ts
@@ -9,4 +9,4 @@ export const IssueDate = 'Issue Date';
 export const BillID = 'Bill ID';
 export const Status = 'Status';
 export const DueDate = 'Due Date';
-export const DownloadAs = 'Download bill as a PDF';
+export const DownloadAs = 'Download bill as PDF';
