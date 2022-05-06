93a8a1b3e27cdb6902ee8104ab0bb7a2609ddb95
omkargarde <omyagarde@gmail.com>
2022-05-06 11:30:36 +0530

    1st commit

--- src/app/components/productDetails/component/service-share-this-page/service-share-this-page.component.ts
+++ src/app/components/productDetails/component/service-share-this-page/service-share-this-page.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { CmsComponentData } from '@spartacus/storefront';
 import { Observable } from 'rxjs';
 import { RegisterServicesComponent } from '../../productDetails.Model';
-
 @Component({
   selector: 'app-service-share-this-page',
   templateUrl: './service-share-this-page.component.html'
